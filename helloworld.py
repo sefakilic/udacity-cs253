57f10291c36f0dc0a22cbe8f75915af2448d7e9a
Sefa <sefakilic@gmail.com>
2012-08-28 22:56:57 -0400

    usersignup

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,7 @@
 import webapp2
 import rot13
+import usersignup
+
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
@@ -7,5 +9,7 @@ class MainPage(webapp2.RequestHandler):
         self.response.write("Hello, Udacity!")
 
 app = webapp2.WSGIApplication([("/", MainPage),
-                               ("/rot13", rot13.Rot13)],
+                               ("/rot13", rot13.Rot13),
+                               ("/usersignup", usersignup.UserSignup),
+                               ("/welcome", usersignup.Welcome),],
                               debug=True)
