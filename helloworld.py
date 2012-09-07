22ce895e69abeb379b34b0410077749308b3033e
Sefa <sefakilic@gmail.com>
2012-09-07 00:51:33 -0400

    handling final /

--- helloworld.py
+++ helloworld.py
@@ -8,15 +8,15 @@ class MainPage(webapp2.RequestHandler):
         self.response.headers["Content-Type"] = "text/plain"
         self.response.write("Hello, Udacity!")
 
-app = webapp2.WSGIApplication([("/", MainPage),
-                               ("/rot13", rot13.Rot13),
-                               ("/blog/signup", signup.Signup),
-                               ("/blog/login", signup.Login),
-                               ("/blog/logout", signup.Logout),
-                               ("/blog/welcome", signup.Welcome),
-                               ("/blog", blog.Blog),
-                               ("/blog/([0-9]+)", blog.Post),
-                               ("/blog/newpost", blog.NewPost),
+app = webapp2.WSGIApplication([("/?", MainPage),
+                               ("/rot13/?", rot13.Rot13),
+                               ("/blog/signup/?", signup.Signup),
+                               ("/blog/login/?", signup.Login),
+                               ("/blog/logout/?", signup.Logout),
+                               ("/blog/welcome/?", signup.Welcome),
+                               ("/blog/?", blog.Blog),
+                               ("/blog/([0-9]+)/?", blog.Post),
+                               ("/blog/newpost/?", blog.NewPost),
                               #json
                                (("/blog/.json"), blog.BlogJSON),
                                (("/blog/([0-9]+).json"), blog.PostJSON)],
